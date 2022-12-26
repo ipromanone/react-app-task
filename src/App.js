08f843754f8049c624670ed6cec9a3fe76d4637b
IPRomanone <ipromanone@gmail.com>
2022-12-26 22:23:26 +0300

    Created and styled task components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import './App.css';
+import Costs from "./components/Costs";
 
 function App() {
+    const costs = [
+        {
+            date: new Date(2021,2,12),
+            description: 'Холодильник',
+            amount: 999.99,
+        },
+        {
+            date: new Date(2021,11,25),
+            description: 'MacBook',
+            amount: 1254.72,
+        },
+        {
+            date: new Date(2021,5,1),
+            description: 'Джинсы',
+            amount: 49.99,
+        },
+    ]
+
+
   return (
     <div>
-      <h1>React-app-task</h1>
+        <h1>React-app-task</h1>
+        <Costs costs={costs}></Costs>
     </div>
   );
 }
