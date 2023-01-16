a00e3ddbcace08b50968ace75a78712fbed23071
IPRomanone <ipromanone@gmail.com>
2023-01-16 22:59:15 +0300

    Created a CostsFilter component and added a year selector

--- src/components/Costs/Costs.js
+++ src/components/Costs/Costs.js
@@ -1,19 +1,32 @@
 import CostItem from "./CostItem";
 import './Costs.css'
 import Card from "../UI/Card";
+import CostsFilter from "./CostsFilter";
+import React, {useState} from "react";
+
+
 
 const Costs = props => {
 
+    const [selectedYear, setSelectedYear] = useState('2021')
+    const yearChangeHandler = (year) => {
+        setSelectedYear(year);
+
+    }
+
     return (
-        <Card className='costs'>
-            <CostItem date={props.costs[0].date} description={props.costs[0].description}
-                      amount={props.costs[0].amount}></CostItem>
-            <CostItem date={props.costs[1].date} description={props.costs[1].description}
-                      amount={props.costs[1].amount}></CostItem>
-            <CostItem date={props.costs[2].date} description={props.costs[2].description}
-                      amount={props.costs[2].amount}></CostItem>
-        </Card>
-        )
+        <div>
+            <Card className='costs'>
+                <CostsFilter year={selectedYear} onChangeYear={yearChangeHandler}></CostsFilter>
+                <CostItem date={props.costs[0].date} description={props.costs[0].description}
+                        amount={props.costs[0].amount}></CostItem>
+                <CostItem date={props.costs[1].date} description={props.costs[1].description}
+                        amount={props.costs[1].amount}></CostItem>
+                <CostItem date={props.costs[2].date} description={props.costs[2].description}
+                        amount={props.costs[2].amount}></CostItem>
+            </Card>
+        </div>
+    )
 };
 
 export default Costs;
